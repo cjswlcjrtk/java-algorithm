eee3cddd80c7192dec3c1dfc74de0b8467d14072
cjswlcjrtk <cjswlcjrtk@naver.com>
2019-08-14 20:39:43 +0900

    190814 20:39

--- homework02/ArithmeticCalculator.java
+++ homework02/ArithmeticCalculator.java
@@ -1,9 +1,41 @@
 package homework02;
+/**
+프로그램을 하나 개발해야 합니다.
+교수가 요청하기를 브라우저에서 사용할 계산기를 만들어 달랍니다.
+복잡할 필요는 없고, 사칙연산 가능하고 나눗셈은
+몫과 나머지값으로 출력하면 된답니다.
+출력결과물은 예를 들어
+5 + 5 = 10
+2 - 7 = -5
+이렇게 되고,
+10 / 3 = 3 [1] 나눗셈은 이렇게 나오면 된답니다.
+그러면 []값이 나머지인지는 교수가 이미 알고 있겠답니다.
+ */
+import java.util.Scanner;
 
 public class ArithmeticCalculator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner scanner = new Scanner(System.in);
+		int number1 = 0;
+		int number2 = 0;
+		String culc = "";
+		number1 = scanner.nextInt();
+		System.out.println("덧셈 뺼셈 곱셈 나눗셈중 원하는 연산을 넣어주세요");
+		culc = scanner.next();
+		number2 = scanner.nextInt();
+		int eval=0;
+		switch(culc){
+		case "+": eval=number1 + number2; break;
+		case "-": eval=number1 - number2; break;
+		case "*": eval=number1 * number2; break;
+		case "/": eval=number1 / number2; break;
+			
+		}
+		System.out.println(number1 + culc + number2 + "=" + eval);
+		
+		
 
 	}
 
--- homework02/TimeCalc.java
+++ homework02/TimeCalc.java
@@ -1,9 +1,41 @@
 package homework02;
 
+import java.util.Scanner;
+
+/**
+ * 	To.개발자님께
+	초단위로 알려주는 시험이 있는데
+	이 값을 몇시간 몇분 몇초인지 계속 계산해야 해서요.
+	혹시 초값을 입력받으면 자동으로 계산되는 프로그램을 만들어 줄 수 있을까요?
+	그러니까 500초 이면 뭐 몇시간 몇분 몇초라고 보여주면 좋겠죠.
+ */
 public class TimeCalc {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int second = 0;
+		int minute = 0;
+		int hour = 0;
+		Scanner scanner = new Scanner(System.in);
+		second = scanner.nextInt();
+		if(second < 60) {
+//			System.out.printf("%s 초입니다.", second);			
+		}else if(second < 3600) {
+			minute = second/60;
+			second = second - minute*60;
+//			System.out.printf("%s 분 %d초입니다.", minute, second);					
+		}else{
+			minute = second/60;
+			System.out.println(minute);
+			hour = minute/60;
+			minute = minute - hour*60;
+			second = second - hour*3600 - minute*60;
+//			System.out.printf("%s 시 %d 분 %f초입니다.",hour, minute, second);	
+			
+			
+		}
+//		System.out.printf("%s 시 %d 분 %f초입니다.",hour, minute, second);	\
+		System.out.println(hour + "시" + minute + "분" + second + "초");
+		
 
 	}
 
